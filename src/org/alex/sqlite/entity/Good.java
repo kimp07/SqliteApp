50b513c33263d0ef4febbced9a7932f1f92fd6c8
zamdirit <zamdirit@zamit.generalfresh.local>
2020-11-16 11:55:41 +0200

    Proj started

---
+++ src/org/alex/sqlite/entity/Good.java
@@ -0,0 +1,66 @@
+package org.alex.sqlite.entity;
+
+import org.alex.sqliteapp.db.EntityField;
+import org.alex.sqliteapp.db.EntityObject;
+
+@EntityObject(
+		query = "SELECT * FROM GOODS",
+		countQuery = "SELECT count(*) FROM GOODS"
+		)
+public class Good {
+
+	@EntityField(fieldName = "_id")
+	private Long id;
+	@EntityField(fieldName = "NAME")
+	private String name;
+	@EntityField(fieldName = "FULLNAME")
+	private String fullName;
+	@EntityField(fieldName = "VAT")
+	private Integer vat;
+	@EntityField(fieldName = "BARCODE")
+	private String barCode;
+
+	public Good() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public Integer getVat() {
+		return vat;
+	}
+
+	public void setVat(Integer vat) {
+		this.vat = vat;
+	}
+
+	public String getBarCode() {
+		return barCode;
+	}
+
+	public void setBarCode(String barCode) {
+		this.barCode = barCode;
+	};
+
+}
---
+++ src/org/alex/sqliteapp/db/EntityField.java
@@ -0,0 +1,13 @@
+package org.alex.sqliteapp.db;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(value = ElementType.FIELD)
+@Retention(value = RetentionPolicy.RUNTIME)
+public @interface EntityField {
+	
+	String fieldName();
+}
